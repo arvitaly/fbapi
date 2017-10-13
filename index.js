befc167c37bdc620978a4f7111dbe5d8d8650627
Vitaly Arkhipov <arvitaly@list.ru>
2017-10-13 09:35:16 +0300

    add exports

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Client_1 = require("./Client");
+var Client_2 = require("./Client");
+exports.Client = Client_2.default;
 exports.default = (opts) => {
     return new Client_1.default(opts);
 };
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export * from "./typings";
 import Client, { IOpts } from "./Client";
 import { IClient } from "./typings";
+export { default as Client } from "./Client";
 export default (opts?: IOpts): IClient => {
     return new Client(opts) as any;
 };
